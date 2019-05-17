abb6e6c83c1f7f3a561dba9f357415b27055248c
Kseniya <kseniya.baranova.2000.14@gmail.com>
2019-05-17 14:28:05 +0700

    Update models

--- catalog/models.py
+++ catalog/models.py
@@ -3,10 +3,8 @@ from __future__ import unicode_literals
 
 from gettext import Catalog
 
-from django.db import models
-
 # Create your models here.
-
+from IPython.core.debugger import Tracer
 from django.db import models
 
 
@@ -14,27 +12,19 @@ class Category(models.Model):
     name = models.CharField(max_length=20)
     parent = models.IntegerField(default=0)
     url = models.CharField(max_length=30)
-    image = models.CharField(max_length=30)
+    image = models.CharField(max_length=30, blank=True, null=True)
 
     def __str__(self):
-        return [self.name, self.url, self.image]
+        return self.name
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=20),
-    description = models.CharField(max_length=200)
-    url = models.CharField(max_length=30)
-    image = models.CharField(max_length=30, default='')
-    count = models.IntegerField
-    price = models.IntegerField
+    name = models.CharField(max_length=20, null=True)
+    category = models.ManyToManyField(Category)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    image = models.CharField(max_length=30, null=True, blank=True)
+    count = models.IntegerField(null=True)
+    price = models.IntegerField(null=True)
 
     def __str__(self):
-        return [self.name, self.image]
-
-
-class Relation(models.Model):
-    product_id = models.ForeignKey(Product)
-    category_id = models.ForeignKey(Category)
-
-    def __str__(self):
-        return [self.product_id, self.category_id]
+        return self.name
--- catalog/views.py
+++ catalog/views.py
@@ -4,14 +4,29 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Category
+from .models import Product
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+def products():
+    list = Product.objects.all()
+    paginator = Paginator(list, 12)
+    page = request.GET.get('page')
+    try:
 
 
 def catalog(request):
-    pass
+    category = Category.objects.filter(parent=0)
+
 
 def category(request, category):
     id = request.GET.get("page", "1")
-    pass
+    category = Category.objects.get(name=category).id
+    subcategory = Category.objects.filter(parent=category)
+
 
 def subcategory(request, category, subcategory):
     id = request.GET.get("page", "1")
