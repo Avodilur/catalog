3ed5c96959ce82dfa80e8c096cc660f2f9b399d7
Kseniya <kseniya.baranova.2000.14@gmail.com>
2019-05-20 17:51:37 +0700

    Upgrage Templates

--- catalog/views.py
+++ catalog/views.py
@@ -4,12 +4,24 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from collections import OrderedDict
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from .models import Category
 from .models import Product
+
+
+def example(request):
+    path = request.path.split('/')[:-1]
+    route = {}
+    arr = path[2:]
+    arr.reverse()
+    for item in arr:
+        route.update([(Category.objects.get(url=item).name, '/'.join(path))])
+        path.pop()
+    return route
 
 
 def catalog(request):
@@ -23,6 +35,9 @@ def catalog(request):
         products = paginator.page(1)
     except EmptyPage:
         products = paginator.page(paginator.num_pages)
+    for item in products:
+        if item.image is None:
+            item.image = Category.objects.get(id=list(item.category.values())[1]['id'], parent=None).image
     return render(request, 'catalog/catalog.html', {'categories': category, 'products': products})
 
 
@@ -34,7 +49,7 @@ def category(request, category, subcategory=''):
         subcategory = Category.objects.get(url=subcategory)
         list_products = Product.objects.filter(category=subcategory)
     else:
-        list_products = Category.objects.filter(category=category)
+        list_products = Product.objects.filter(category=category)
     paginator = Paginator(list_products, 12)
     page = request.GET.get('page', 1)
     try:
@@ -43,5 +58,12 @@ def category(request, category, subcategory=''):
         products = paginator.page(1)
     except EmptyPage:
         products = paginator.page(paginator.num_pages)
+    route = example(request)
+    route = OrderedDict(reversed(list(route.items())))
+    for item in products:
+        if item.image is None:
+            item.image = category.image
     return render(request, 'catalog/category.html', {'subcategories': subcategories, 'products': products,
-                                                     'category': category_url})
+                                                     'category': category_url, 'route': route})
+
+
